0623a676705892ab667d733bcb2cbaf717d5e20b
cchinolla <cmchinolla@miners.utep.edu>
2021-01-12 21:08:04 -0700

    editing code

--- src/wdInterruptHandler.c
+++ src/wdInterruptHandler.c
@@ -1,33 +1,7 @@
 #include <msp430.h>
-#include "stateMachine.h"
+#include "StateMachine.h"
 
 void __interrupt_vec(WDT_VECTOR) WDT(){//250 interrupts/sec
 
-  static char counter=0;
-  switch(state){
-  case 1:
-    if(++counter == 50){
-      state_advance();
-      counter = 0;
-    }
-  case 2:
-    if(++counter == 125){
-      state_advance();
-      counter = 0;
-    }
-  case 3:
-    state_advance();
-    counter = 0;
-  case 4:
-    if(++counter < 62){
-      dim25();
-    }else if(++counter < 125){
-      dim50();
-    }else{
-      if(++counter == 250){
-	counter = 0;
-      }else{
-	dim75();
-      }
   
   }
